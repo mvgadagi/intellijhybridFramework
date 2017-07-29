44d649e3fb71182f35a8378c1d00d72b664b94f7
mvgadagi <mvgadagi@gmail.com>
2017-07-29 12:28:40 +0530

    This is the first Commit

---
+++ src/main/java/com/hybridFramework/testBase/TestBase.java
@@ -0,0 +1,39 @@
+package com.hybridFramework.testBase;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+
+	WebDriver driver;
+
+	public void getBrowser(String browser) {
+
+		System.out.println(System.getProperty("user.dir"));
+		if (System.getProperty("os.name").contains("Window")) {
+			if (browser.equalsIgnoreCase("firefox")) {
+				System.setProperty("webdriver.gecko.driver",
+						System.getProperty("user.dir") + "/drivers/geckodriver.exe");
+			} else if (browser.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("usr.dir") + "/drivers/chromedriver.exe");
+			}
+		}
+
+		else if (System.getProperty("os.name").contains("Mac")) {
+			if (browser.equalsIgnoreCase("firefox")) {
+				System.setProperty("webdriver.firefox.marionette",
+						System.getProperty("user.dir") + "/drivers/geckodriver");
+			} else if (browser.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver", System.getProperty("usr.dir") + "/drivers/chromedriver");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		TestBase test = new TestBase();
+		test.getBrowser("firefox");
+	}
+
+}
